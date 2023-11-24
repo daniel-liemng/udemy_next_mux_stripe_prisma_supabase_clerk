4a5d6c5449cb77c2df13d9fecbb75711bb03c144
Daniel <daniel.liemng@gmail.com>
2023-11-24 18:25:52 -0500

    category form - combobox

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { redirect } from 'next/navigation';
 import TitleForm from './_components/title-form';
 import DescriptionForm from './_components/description-form';
 import ImageForm from './_components/image-form';
+import CategoryForm from './_components/category-form';
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -19,6 +20,17 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
       id: params.courseId,
     },
   });
+
+  const categories = await prismadb.category.findMany({
+    orderBy: {
+      name: 'asc',
+    },
+  });
+
+  const formattedCategories = categories?.map((category) => ({
+    label: category.name,
+    value: category.id,
+  }));
 
   if (!course) {
     return redirect('/');
@@ -56,8 +68,16 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           </div>
 
           <TitleForm initialData={course} courseId={course.id} />
+
           <DescriptionForm initialData={course} courseId={course.id} />
+
           <ImageForm initialData={course} courseId={course.id} />
+
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={formattedCategories}
+          />
         </div>
       </div>
     </div>
---
+++ scripts/seed.ts
@@ -0,0 +1,27 @@
+const { PrismaClient } = require('@prisma/client');
+
+const database = new PrismaClient();
+
+const main = async () => {
+  try {
+    await database.category.createMany({
+      data: [
+        { name: 'Computer Science' },
+        { name: 'Music' },
+        { name: 'Fitness' },
+        { name: 'Photography' },
+        { name: 'Accounting' },
+        { name: 'Engineering' },
+        { name: 'Filming' },
+      ],
+    });
+
+    console.log('Success');
+  } catch (err) {
+    console.log('Error seeding the database categories', err);
+  } finally {
+    await database.$disconnect();
+  }
+};
+
+main();
