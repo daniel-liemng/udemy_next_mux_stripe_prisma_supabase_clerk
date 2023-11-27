90b8af5cda82c23dab01efe5e515dda78a4ad392
Daniel <daniel.liemng@gmail.com>
2023-11-27 16:47:26 -0500

    react confetti (publish course)

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/course-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/course-actions.tsx
@@ -2,6 +2,7 @@
 
 import ConfirmModal from '@/components/modals/confirm-modal';
 import { Button } from '@/components/ui/button';
+import { useConfettiStore } from '@/hooks/use-confetti-store';
 import axios from 'axios';
 import { Trash } from 'lucide-react';
 import { useRouter } from 'next/navigation';
@@ -21,6 +22,8 @@ const CourseActions = ({
 }: CourseActionsProps) => {
   const router = useRouter();
 
+  const confetti = useConfettiStore();
+
   const [isLoading, setIsLoading] = useState(false);
 
   // Publish/unpublish a chapter
@@ -36,6 +39,7 @@ const CourseActions = ({
         await axios.patch(`/api/courses/${courseId}/publish`);
 
         toast.success('Course published');
+        confetti.onOpen();
       }
 
       router.refresh();
