b7208dbce2a669bd9d7f007360452b8befb3a87b
Daniel <daniel.liemng@gmail.com>
2023-11-28 13:21:46 -0500

    search page - category query

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,18 @@
-const SearchPage = () => {
-  return <div>SearchPage</div>;
+import { prismadb } from '@/lib/prismadb';
+import Categories from './_components/categories';
+
+const SearchPage = async () => {
+  const categories = await prismadb.category.findMany({
+    orderBy: {
+      name: 'asc',
+    },
+  });
+
+  return (
+    <div className='p-6'>
+      <Categories items={categories} />
+    </div>
+  );
 };
 
 export default SearchPage;
